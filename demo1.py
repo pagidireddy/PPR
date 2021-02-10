a86148644bf7dd8d569abf54245d7d9125332896
pagidireddy <78584485+pagidireddy@users.noreply.github.com>
2021-02-10 17:47:48 +0530

    Create demo1.py

--- demo1.py
+++ demo1.py
@@ -1,1 +1,13 @@
-a,b=1,2
+x = [2, 4, 3, 1, 5]
+n = len(x)
+
+# Traverse through all list elements
+for i in range(n):
+# Traverse the list from 0 to n-i-1
+# (The last element will already be in place after first pass, so no need to re-check)
+    for j in range(0, n-i-1):
+
+    # Swap if current element is greater than next
+        if x[j] > x[j+1]:
+            x[j], x[j+1] = x[j+1], x[j]
+print(x)
